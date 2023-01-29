c0b148828e587babeae9ea6e721b4090a7295e1a
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2023-01-29 12:51:50 +0530

    added new fixes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,17 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react"
-}
+  stories: ["../src/**/*.stories.tsx"],
+  // Add any Storybook addons you want here: https://storybook.js.org/addons/
+  addons: ["@storybook/addon-essentials", "@storybook/addon-postcss"],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: [["react-app", { flow: false, typescript: true }]],
+      },
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+
+    return config;
+  },
+};
